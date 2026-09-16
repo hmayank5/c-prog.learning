cfad03b49b9de743ed99a281456af726a31aa918
hmayank5 <mayanktanmay1@gmail.com>
2026-09-16 23:10:57 +0530

    dftd with c prog

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# c-prog.learning
+C Programming 🚀
+
+Learning C from 0 → Strong Foundations.
+
+This repo contains my:
+
+* 📚 Concepts
+* 💻 Practice programs
+* 🧩 Problem solving
+* 🛠️ Mini projects
+
+
+Topics Covered
+
+* Basics of C
+* Variables & Data Types
+* Operators
+* Input & Output
+* Conditional Statements
+* Loops
+* Functions
+* Arrays
+* Strings
+* Pointers
+* Structures & Unions
+* Dynamic Memory Allocation
+* File Handling
+* Recursion
+* Preprocessor & Header Files
+* Practice Problems
+* Mini Projects
---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    float b = 20.5;
+    char c = 'A';
+ return 0;
+}
+//variables is name of memory location which can store data.
+//rules of variable naming in C:
+//1. Variable names can only contain letters, digits, and underscores.
+//2. Variable names must begin with a letter or an underscore. 
+//3. Variable names are case-sensitive.
+//4. Variable names cannot be a reserved keyword in C.
+//5 . Variable names should be meaningful and descriptive to indicate the purpose of the variable.
