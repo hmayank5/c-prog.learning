a4ab03974524c7ecb79a488c3af8d8e302c15c19
hmayank5 <mayanktanmay1@gmail.com>
2026-09-17 22:54:57 +0530

    learnt abt keywords and tracing the code

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-int main() {
-    int a = 10;
+#include <stdio.h> //preprocessor directive used to include the standard input-output library in the program
+int main() {       //main function is the entry point of the program where execution starts.evrtg here is case sensitive
+    int a = 10;    //semi colon is like full stop in c.whihc says now this line ends.
     float b = 20.5;
     char c = 'A';
- return 0;
+ return 0;   // this is like saying 0 error.and like tht says this is end of my programming. 
 }
 //variables is name of memory location which can store data.
 //rules of variable naming in C:
@@ -12,3 +12,23 @@ int main() {
 //3. Variable names are case-sensitive.
 //4. Variable names cannot be a reserved keyword in C.
 //5 . Variable names should be meaningful and descriptive to indicate the purpose of the variable.
+
+
+#include <stdio.h>
+int main() {
+    int age = 10;
+    float pi= 3.14;
+    char c = 'A';
+    return 0;
+}   
+//int is used to store integer values
+//float is used to store decimal values
+//char is used to store single character values
+
+
+
+//keywords are reserved words in C that have a predefined meaning and cannot be used as variable names. Some examples of keywords in C include int, float, char, return, if, else, while, for, etc.
+
+
+
+
